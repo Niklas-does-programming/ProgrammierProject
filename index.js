09930a4f91a82ea8d36e1624a8cb397ba9fe3f89
Niklas-does-programming <84233627+Niklas-does-programming@users.noreply.github.com>
2023-02-05 15:49:27 +0100

    prompt extension

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 // and the inputs will be forwarded to the
 // responsible functions
 
-
+const prompt = require("prompt-sync")();
 // import * as fs from 'fs'; // importiere Methoden aus 'fs'
 // const file = fs.readFileSync('./data.json'); // JSON-Datei einlesen
 // const data = JSON.parse(file); // JSON-Daten --> JS Array/Objekte
 
 
 // read data from file
-
+console.log("hallo");
+let test = prompt('lalala');
+console.log(test);
 
 // programm loop
