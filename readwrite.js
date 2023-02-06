52da5fd2032f42a965502c0e72c3e3d510709148
Niklas-does-programming <84233627+Niklas-does-programming@users.noreply.github.com>
2023-02-06 10:47:51 +0100

    push neues package

--- readwrite.js
+++ readwrite.js
@@ -2,7 +2,11 @@
 // functions for saving the data
 // and reading it from a JSON file
 
+let jsonFile = require('jsonfile');
 
+for (i = 0; i < 11; i++) {
+    jsonFile.writeFile('loop.json', "id :" + i + " square :" + i * i);
+}
 // read data
 
 
