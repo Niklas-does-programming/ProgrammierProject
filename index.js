359cb3ba94423234e7c59061fa5fca67ee2c22b0
romanVbosse <124140978+romanVbosse@users.noreply.github.com>
2023-02-15 11:25:20 +0100

    addition askQuestion

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // main file
-// here the main programm loop will take place 
+// here the main programm loop will take place
 // and the inputs will be forwarded to the
 // responsible functions
-import {programmState, question, multipleChoice} from './utils.js';
-import { saveData, readData } from './readwrite.js';
+import { programmState, question, multipleChoice } from "./utils.js";
+import { saveData, readData } from "./readwrite.js";
 
-import psp from 'prompt-sync-plus';
+import psp from "prompt-sync-plus";
 const prompt = psp();
 
-import {handleManagement} from './management.js';
-import {handleTraining} from './training.js';
+import { handleManagement } from "./management.js";
+import { handleTraining } from "./training.js";
 
 // String def
-let mainString = "Sie befinden sich im Hauptmenü, was möchten Sie tun?\n" + 
-                   "[1] Zum Verwaltungsmenü\n" + 
-                   "[2] Zum Anwendungsmenü\n" +
-                   "[exit] Beenden der App\n";
+let mainString =
+  "Sie befinden sich im Hauptmenü, was möchten Sie tun?\n" +
+  "[1] Zum Verwaltungsmenü\n" +
+  "[2] Zum Anwendungsmenü\n" +
+  "[exit] Beenden der App\n";
 //////////////
 
-let ps = new programmState("main",[],[]); //[]dummy
+let ps = new programmState("main", [], []); //[]dummy
 
 // const dic = {"deshalb" : true,"weil" : true, "Baum" : false};
 
@@ -27,37 +28,35 @@ let ps = new programmState("main",[],[]); //[]dummy
 // let q3 = new question("Frage", "weshalb", "deshalb", "Deutsch", 0, 0);
 // let q4 = new multipleChoice("Mult-Frage", "weshalb", {"deshalb" : true,"weil" : true, "Baum" : false}, "Deutsch", 0, 0);
 
-
 // let ps2 = new programmState("main", [q1, q2, q3, q4], ["Mathe", "Deutsch"]);
-let ps2 = readData("defaultPS")
+let ps2 = readData("defaultPS");
 
 // Start of Programm
 console.log("Wilkommen zu der Lernapp");
-console.log("Mit exit kommt man zurück zum Hauptmenü bzw. beendet das Programm");
+console.log(
+  "Mit exit kommt man zurück zum Hauptmenü bzw. beendet das Programm"
+);
 
 let input = prompt(mainString);
 
 // programm loop
 
-while(input !== "exit"){
-
-    switch(input){
-        case "1":
-            ps.menu = "ver";
-            console.clear();
-            handleManagement(ps2);
-            break;
-        case "2":
-            ps.menu = "anw";
-            console.clear();
-            handleTraining(ps2);
-            break;
-        default:
-            console.log("Ungültige Eingabe");
-            break;
-
-    }
-    input = prompt(mainString);
-}   
-//console.log(ps2);/////////////////////////////////////////////
-
+while (input !== "exit") {
+  switch (input) {
+    case "1":
+      ps.menu = "ver";
+      console.clear();
+      handleManagement(ps2);
+      break;
+    case "2":
+      ps.menu = "anw";
+      console.clear();
+      handleTraining(ps2);
+      break;
+    default:
+      console.log("Ungültige Eingabe");
+      break;
+  }
+  input = prompt(mainString);
+}
+//console.log(ps2); /////////////////////////////////////////////
--- training.js
+++ training.js
@@ -1,82 +1,96 @@
 // training with questions
-// functions for the application mode 
-// of the software 
+// functions for the application mode
+// of the software
 
-import {programmState,question,select} from "./utils.js";
+import { programmState, question, select } from "./utils.js";
 
-import psp from 'prompt-sync-plus';
+import psp from "prompt-sync-plus";
 const prompt = psp();
 
 //String Def
-let trainingString = "Sie befinden sich im Anwendungsmenü, was möchten Sie tun?\n" + 
-                   "[1] Eine Kategorie für Fragen Auswählen\n" + 
-                   "[2] Fragen aus allen Kategorie\n" +
-                   "[exit] zurück zum Hauptmenü\n";
+let trainingString =
+  "Sie befinden sich im Anwendungsmenü, was möchten Sie tun?\n" +
+  "[1] Eine Kategorie für Fragen Auswählen\n" +
+  "[2] Fragen aus allen Kategorie\n" +
+  "[exit] zurück zum Hauptmenü\n";
 ////////////////////
 
-export function handleTraining(ps){
-    let input = prompt(trainingString);
-    switch(input){
-        case "1":
-            let cat = chooseCategory(ps.categoryArray);
-            let question = getQuestions(ps,cat);
-            askQuestion(ps,question);
-            break;
-        case "2":
-
-            break;
-        case "exit":
-            break;
-        default:
-            console.log("Ungültige Eingabe");
-            handleTraining(ps);
-            break;
-    }
-    console.clear();
+export function handleTraining(ps) {
+  let input = prompt(trainingString);
+  switch (input) {
+    case "1":
+      let cat = chooseCategory(ps.categoryArray);
+      let question = getQuestions(ps, cat);
+      askQuestion(ps, question);
+      //console.log(ps);
+      break;
+    case "2":
+      break;
+    case "exit":
+      break;
+    default:
+      console.log("Ungültige Eingabe");
+      handleTraining(ps);
+      break;
+  }
+  console.clear();
 }
 
 // get questions
-function getQuestions(ps,category){
-    let temp = select(ps,category);
-    return temp;
+function getQuestions(ps, category) {
+  let temp = select(ps, category);
+  return temp;
 }
 
 // number of questions to be asked
-function numQuestions(ps,array,category){
-    let num = prompt(`Anzahl an Fragen zum Thema ${category}: ${array.length}`);
-    // TODO function to choose questions
-    return num;
+function numQuestions(ps, array, category) {
+  let num = prompt(`Anzahl an Fragen zum Thema ${category}: ${array.length}`);
+  // TODO function to choose questions
+  return num;
 }
 
 // choose category
-function chooseCategory(array){
-    let categoryString = "";
-    for(let i = 0;i<array.length;i++){
-        categoryString = categoryString + `[${i+1}] ${array[i]}\n`;
-    }
-    categoryString = "Welche Kategorie möchten Sie auswählen?\n" + categoryString;
-    let index = parseFloat(prompt(categoryString))-1;
-    return array[index];
+function chooseCategory(array) {
+  let categoryString = "";
+  for (let i = 0; i < array.length; i++) {
+    categoryString = categoryString + `[${i + 1}] ${array[i]}\n`;
+  }
+  categoryString = "Welche Kategorie möchten Sie auswählen?\n" + categoryString;
+  let index = parseFloat(prompt(categoryString)) - 1;
+  return array[index];
 }
 
 // ask questions
-function askQuestion(ps,array){
-    let ask = array;
-    for(let i = 0;i<ask.length;i++){
-        let ans = prompt(`${ask[i].questionText}\n`);
-        if(ans === ask[i].answerText){
-            ask[i].asked += 1;
-            console.log("Die Anwort war richtig");
-        }
-        else{
-            ask[i].asked += 1;
-            ask[i].wrong += 1;
-            console.log("Die Anwort war nicht richtig");
+function askQuestion(ps, array) {
+  let ask = array;
+  let type;
+  for (let k = 0; k < ask.length; k++) {
+    type = ask[k].type;
+    switch (type) {
+      case "Frage":
+        let ans = prompt(`${ask[k].questionText}\n`);
+        if (ans === ask[k].answerText) {
+          ask[k].asked += 1;
+          console.log("Die Anwort war richtig");
+        } else {
+          ask[k].asked += 1;
+          ask[k].wrong += 1;
+          console.log("Die Anwort war nicht richtig");
         }
         console.clear();
+        break;
+      case "Mult-Frage":
+        console.log(ask[k].questionText);
+        let que;
+        for (let y = 0; y < ask[k].answerDic.length; y++) {
+          que = que + ask[k].answerDic[y];
+        }
+        console.log(que);
+        break;
     }
-    
-    for(let j = 0;j<ask.length;j++){
-        ps.questionArray.push(ask[j]);
-    }
+  }
+
+  for (let z = 0; z < ask.length; z++) {
+    ps.questionArray.push(ask[z]);
+  }
 }
